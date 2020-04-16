ca9f9c39d0e52025e419246b6f3eca0e3b123212
jongwoo <jongwoo328@gmail.com>
2020-04-16 14:00:17 +0900

    0416 | date display & price logic update

--- hotdealapp/data.py
+++ hotdealapp/data.py
@@ -37,7 +37,8 @@ def get_fmk(target="title_content", key="만두"):
     parsed_data = BeautifulSoup(request.text, "html.parser")
 
     # date_list 만들기
-    date_list = parsed_data.select("#bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li > div > div:nth-child(5) > span.regdate")
+    #bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li:nth-child(4) > div > div:nth-child(4) > span.regdate
+    date_list = parsed_data.select("#bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li > div > div:last-child > span.regdate")
     date_list = list(map(lambda x: x.text.strip(), date_list))
 
     # title_list 만들고 날짜, 만료여부 저장
@@ -45,12 +46,20 @@ def get_fmk(target="title_content", key="만두"):
     title_list = parsed_data.select("#bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li > div > h3 > a")
     
     today_date = datetime.today()
-    print(
-        today_date - datetime.strptime(date_list[0], "%Y.%m.%d"),
-        timedelta(days=5)
-    )
+    # print(
+    #     today_date - datetime.strptime(date_list[0], "%Y.%m.%d"),
+    #     timedelta(days=5)
+    # )
+    
     for index, title in enumerate(title_list):
-        if title.attrs["class"][0] == "hotdeal_var8" and (today_date - datetime.strptime(date_list[index], "%Y.%m.%d")) < timedelta(days=days_delta):
+        # if title.attrs["class"][0] == "hotdeal_var8":
+        try:
+            article_date = today_date - datetime.strptime(date_list[index], "%Y.%m.%d")
+            
+        except ValueError:
+            article_date = today_date - today_date
+            valid_title_idx.append(index)
+        if article_date < timedelta(days=days_delta):
             valid_title_idx.append(index)
     title_list = list(map(get_title_fmk, title_list))
     
@@ -59,17 +68,21 @@ def get_fmk(target="title_content", key="만두"):
     link_list = list(map(get_article_id_fmk, link_list))
 
     # price_list 만들기
-    price_list = parsed_data.select("#bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li > div > div.hotdeal_info > span:nth-child(2)")
-    price_list = list(map(lambda x: (x.text)[4:], price_list))
+    #bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li:nth-child(4) > div > div.hotdeal_info > span:nth-child(2) > a
+    price_list = parsed_data.select("#bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li > div > div.hotdeal_info > span:nth-child(2) > a")
+    price_list = list(map(lambda x: (x.text), price_list))
     
-
     result = []
     for i in valid_title_idx:
+        try:
+            date = datetime.strptime(date_list[i], "%Y.%m.%d").strftime("%Y-%m-%d")
+        except ValueError:
+            date = date_list[i]
         result.append({
             "title": title_list[i],
             "price": price_list[i],
             "link": link_list[i],
-            "date": datetime.strptime(date_list[i], "%Y.%m.%d").strftime("%Y-%m-%d"),
+            "date": date,
         })
 
     return result
