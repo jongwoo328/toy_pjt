6971ed35b6fa007317b55c903c236d59b83c6f67
김태인 <kti91@likelion.org>
2020-04-16 15:00:32 +0900

    0416 |  update views, templates

--- hotdealapp/views.py
+++ hotdealapp/views.py
@@ -13,18 +13,20 @@ def index(request):
     # 데이터 불러올 로직
     if request.method == "POST":
         form = HotdealForm(request.POST)
-        key = request.POST['key']
-        target = request.POST['target']
-        datas = get_fmk(key=key, target=target)
-        context = {
-            'form': form,
-            'datas': datas,
-        }
-        return render(request, 'hotdealapp/index.html', context)
+        if form.is_valid():
+            key = request.POST['key']
+            target = request.POST['target']
+            datas = get_fmk(key=key, target=target)
+            context = {
+                'form': form,
+                'datas': datas,
+            }
+            return render(request, 'hotdealapp/index.html', context)
     else:
         form = HotdealForm()
     # 핫딜데이터 (이름, url, 가격, 쇼핑몰 정보 보유)
-        context = {
-            'form': form,
-        }
+    context = {
+        'form': form,
+        'datas': None,
+    }
     return render(request, 'hotdealapp/index.html', context)
