86c6599cc0fbb454f23939ea71034c2629a15caa
김태인 <kti91@likelion.org>
2020-04-17 19:41:57 +0900

    0417 | modified models, views

--- hotdealapp/forms.py
+++ hotdealapp/forms.py
@@ -1,13 +1,11 @@
 from django import forms
 from .models import Hotdeal
 
-Choice = ((1, "제목"), (2, "내용"), (3, "제목 + 내용"))
+Choice = ((1, "제목 + 내용"), (2, "제목"))
 
 class HotdealForm(forms.ModelForm):
     
     key = forms.CharField(
-        # label=_("검색"),
-        # help_text="싸게 사고 싶은 상품을 입력해주세요.",
         required=True,
         widget=forms.TextInput(attrs={'name': 'key', 'placeholder': '상품 입력'}),
     )
--- hotdealapp/models.py
+++ hotdealapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Hotdeal(models.Model):
     key = models.CharField(max_length=100)
-    target = models.IntegerField(choices=((1, "제목"), (2, "내용"), (3, "제목 + 내용")), default=1)
+    target = models.IntegerField(choices=((1, "제목 + 내용"), (2, "제목")), default=1)
