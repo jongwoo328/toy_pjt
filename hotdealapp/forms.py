e6fea1a4a755b8462ebcc83ad2999f85b4ae95ee
김태인 <kti91@likelion.org>
2020-04-18 16:06:35 +0900

    0418 | modified search_bar

--- hotdealapp/forms.py
+++ hotdealapp/forms.py
@@ -6,11 +6,13 @@ Choice = ((1, "제목 + 내용"), (2, "제목"))
 class HotdealForm(forms.ModelForm):
     
     key = forms.CharField(
+        label="",
         required=True,
-        widget=forms.TextInput(attrs={'name': 'key', 'placeholder': '상품 입력'}),
+        widget=forms.TextInput(attrs={'name': 'key', 'placeholder': '상품 입력', 'class': 'search_input'}),
     )
-    
-    target = forms.ChoiceField(choices=Choice)
+    # target = forms.ChoiceField(
+    #     label="",
+    #     choices=Choice)
     class Meta:
         model = Hotdeal
-        fields = ['key', 'target']
+        fields = ['key']
