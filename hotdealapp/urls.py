558ab0ef46692ef1f95913d92109a21a1df8c80e
jongwoo <jongwoo328@gmail.com>
2020-04-28 00:36:24 +0900

    0428 | restore urls

--- hotdealapp/urls.py
+++ hotdealapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from hotdeal import settings
+
 
 app_name = 'hotdeal'
 
@@ -8,7 +8,4 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('result/', views.result, name="result"),
 ]
-if not settings.DEBUG:
-      urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+
