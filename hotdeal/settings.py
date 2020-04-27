cfabe4a7a2bb82ed0b2db3c965e59ebaf593a92b
jongwoo <jongwoo328@gmail.com>
2020-04-28 01:11:46 +0900

    0428 | set debug False

--- hotdeal/settings.py
+++ hotdeal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*+xf$z%hd=(&h1062rvdclrnaydpt53)6*6gdu874m6%asocfi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -33,7 +33,7 @@ MIDDLEWARE = [
 ]
 
 # Activate Django-Heroku
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Application definition
 
