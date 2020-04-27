495fa0818515a3da13fa3ee9b45e8bdf2a60252a
jongwoo <jongwoo328@gmail.com>
2020-04-28 00:30:55 +0900

    0428 | update urls 3

--- hotdealapp/urls.py
+++ hotdealapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('result/', views.result, name="result"),
 ]
 if not settings.DEBUG:
-      urlpatterns += staticfiles_urlpatterns()
+      urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
