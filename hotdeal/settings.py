2f15d2035b0d501329ca64d670ae896b542766e2
jongwoo <jongwoo328@gmail.com>
2020-04-27 23:25:58 +0900

    0427 | set DEBUG False

--- hotdeal/settings.py
+++ hotdeal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*+xf$z%hd=(&h1062rvdclrnaydpt53)6*6gdu874m6%asocfi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
