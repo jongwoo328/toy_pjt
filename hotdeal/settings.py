7802b463ca1181c1e6176838c24c38ba773dcd1a
jongwoo <jongwoo328@gmail.com>
2020-04-28 15:17:10 +0900

    0428 | search fixed to GET

--- hotdeal/settings.py
+++ hotdeal/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*+xf$z%hd=(&h1062rvdclrnaydpt53)6*6gdu874m6%asocfi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
